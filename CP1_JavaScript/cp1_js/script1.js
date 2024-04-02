e4fa50e4f8900ee1b445070eb9bfbd580d45e554
Pedro Sampaio <98555328+sampaiopedroh@users.noreply.github.com>
2024-04-02 16:51:52 -0300

    Update script1.js
    Corrigindo e finalizando o JS

--- CP1_JavaScript/cp1_js/script1.js
+++ CP1_JavaScript/cp1_js/script1.js
@@ -1,14 +1,12 @@
-    var nome = window.prompt('Qual o nome do vinho ?');
-    var tipo = window.prompt('Qual o tipo do vinho ?');
-    var safra = window.prompt('Qual a safra do vinho ?');
-    var preco = window.prompt('Qual o preco do vinho');
-function add_vinho () {
-    var novosVinhos = [nome, tipo, safra, preco];
-    console.log(novosVinhos);
+const novoVinho = [nome, tipo, safra, preco];
+function add_vinho() {
+    nome = window.prompt('Qual o nome do vinho');
+    tipo = window.prompt('Qual o tipo do vinho ?');
+    safra = window.prompt('Qual a safra do vinho ?');
+    preco = window.prompt('Qual o preço do vinho ?');
 }
-
-function mostra_catalogo() {
-    var res = document.getElementById('lista_vinho')
+function mostra_vinho() {
+    var res = document.getElementById('vinho')
     res.innerHTML = `<p>O nome do vinho é <mark>${nome}</mark>.</p>`
     res.innerHTML += `<p>O tipo do vinho é <mark>${tipo}</mark>.</p>`
     res.innerHTML += `<p>A safra do vinho é <mark>${safra}</mark>.</p>`
